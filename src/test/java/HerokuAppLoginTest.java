aff4a163f0783fc1334af8b221292032b9f4cd26
Shivadath <jithushivabro8086@gmail.com>
2025-12-29 15:18:02 +0530

    again updated

--- src/test/java/HerokuAppLoginTest.java
+++ src/test/java/HerokuAppLoginTest.java
@@ -40,7 +40,7 @@ public class HerokuAppLoginTest {
         WebElement password = driver.findElement(By.id("password"));
 
         // Enter credentials
-        username.sendKeys("tomsmith");
+        username.sendKeys("tomsmith1");
         password.sendKeys("SuperSecretPassword!");
 
         // Click login
